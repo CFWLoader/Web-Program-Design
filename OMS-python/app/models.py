a5e36df738005b8d1377f57f55176586764f8202
CFWLoader <cfwloader@gmail.com>
2017-04-17 20:04:47 +0800

    db_migrate works.

--- OMS-python/app/models.py
+++ OMS-python/app/models.py
@@ -20,5 +20,15 @@ class User(db.Model):
         return '<User %r>' % self.nickname
 
 
-class Post:
-    pass
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    body = db.Column(db.String(192))
+
+    timestamp = db.Column(db.DateTime)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.body
