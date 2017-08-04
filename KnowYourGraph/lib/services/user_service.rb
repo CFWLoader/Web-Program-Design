60c4465b7901aa8fbc1b3b0b98e7108e5e3619d3
Evan Huang <cfwloader@gmail.com>
2017-08-04 16:41:27 +0800

    Save point.

--- KnowYourGraph/lib/services/user_service.rb
+++ KnowYourGraph/lib/services/user_service.rb
@@ -1,7 +1,11 @@
 module UserService
 
   def check_params params
-    p params.require(:user) #.permit(:password_first)
+    required_params = params.require(:user).permit :password_first, :password_second
+
+    p required_params[:password_first]
+    p required_params[:password_second]
+    
   end
 
 end
