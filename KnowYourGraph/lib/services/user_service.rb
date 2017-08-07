2f5db718a25b75a61d5ed1c39d393475ddf53041
Evan Huang <cfwloader@gmail.com>
2017-08-07 11:21:09 +0800

    Looks like I have to learn Rails for Ajax part.

--- KnowYourGraph/lib/services/user_service.rb
+++ KnowYourGraph/lib/services/user_service.rb
@@ -1,10 +1,14 @@
 module UserService
 
   def check_params params
+
     required_params = params.require(:user).permit :password_first, :password_second
 
-    p required_params[:password_first]
-    p required_params[:password_second]
+    if required_params[:password_first] != required_params[:password_second]
+      return "Confirm password failed!"
+    end
+
+    "OK"
 
   end
 
